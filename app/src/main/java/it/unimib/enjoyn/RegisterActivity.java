d3bab5254eaef745319c7966cd536fe3f5941961
stefanospinelli29 <128166491+stefanospinelli29@users.noreply.github.com>
2023-11-08 22:06:28 +0100

    Aggiunta libreria EmailValidator, aggiunti verifica email e conferma password di RegisterActivity

--- app/src/main/java/it/unimib/enjoyn/RegisterActivity.java
+++ app/src/main/java/it/unimib/enjoyn/RegisterActivity.java
@@ -9,70 +9,101 @@ import android.widget.Button;
 
 import com.google.android.material.textfield.TextInputLayout;
 
+import org.apache.commons.validator.routines.EmailValidator;
+
 public class RegisterActivity extends AppCompatActivity {
     TextInputLayout Textpassword;
+    TextInputLayout TextEmail;
+    TextInputLayout TextConfirmpassword;
     Button ButtonRegister;
+
     private static final String TAG = RegisterActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         Textpassword = (findViewById(R.id.insertPassword));
-        ButtonRegister= findViewById(R.id.buttonRegister);
+        ButtonRegister = findViewById(R.id.buttonRegister);
+        TextEmail = findViewById(R.id.insertEmail);
+        TextConfirmpassword = findViewById(R.id.confirmPassword);
         ButtonRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String password = Textpassword.getEditText().getText().toString();
+                String email = TextEmail.getEditText().getText().toString();
+                String confirmPassword = TextConfirmpassword.getEditText().getText().toString();
+                boolean checkedConfirmPassword= false;
                 boolean checkedPassword = checkPassword(password);
-                Log.d(TAG, "funziona"+checkedPassword);
+                if(checkedPassword){
+                    checkedConfirmPassword= checkConfirmPassword( password, confirmPassword);
+                }
+                boolean checkedEmail = checkEmail(email);
+                Log.d(TAG, "funziona" + checkedPassword);
+                Log.d(TAG, "funzionaE" + checkedEmail);
             }
         });
     }
 
 
-
-    private boolean checkPassword(String password){
+    private boolean checkPassword(String password) {
         boolean passedP;
         boolean number = false;
         boolean specialChar = false;
         boolean capLetter = false;
-        if( password != null && password.length() >= 8){
-            for(int i = 0; i < password.length() ; i++ ){
-                if(password.charAt(i) >= '0' && password.charAt(i) <= '9'){
+        if (password != null && password.length() >= 8) {
+            for (int i = 0; i < password.length(); i++) {
+                if (password.charAt(i) >= '0' && password.charAt(i) <= '9') {
                     number = true;
                 }
-                if(password.charAt(i) >= 'A' && password.charAt(i) <= 'Z'){
+                if (password.charAt(i) >= 'A' && password.charAt(i) <= 'Z') {
                     capLetter = true;
                 }
-                if(password.charAt(i) >= '!' && password.charAt(i) <= '/'){
+                if (password.charAt(i) >= '!' && password.charAt(i) <= '/') {
                     specialChar = true;
                 }
 
             }
 
-        }
-        else{
+        } else {
             Textpassword.setError("@string/tooShortPassword");
             return false;
         }
-        if(!number){
+        if (!number) {
             Textpassword.setError("@string/numberMissingPassword");
-        }
-        else {
-            if(!capLetter){
+        } else {
+            if (!capLetter) {
                 Textpassword.setError("@string/upperCaseMissingPassword");
-            }
-            else{
-                if(!specialChar){
+            } else {
+                if (!specialChar) {
                     Textpassword.setError("@string/specialCharacterMissingPassword");
                 }
             }
 
         }
-        passedP= (number && capLetter && specialChar);
-        if(passedP){
-        Textpassword.setError(null);
+        passedP = (number && capLetter && specialChar);
+        if (passedP) {
+            Textpassword.setError(null);
         }
         return passedP;
+    }
+
+    private boolean checkEmail(String email) {
+        if (!(EmailValidator.getInstance().isValid(email))) {
+            TextEmail.setError("@string/notValidEmail");
+            return false;
+        } else {
+            TextEmail.setError(null);
+            return true;
+        }
+    }
+    private boolean checkConfirmPassword(String password, String confirmPassword) {
+        if (!(confirmPassword.equals(password))) {
+            TextConfirmpassword.setError("@string/PasswordNotEqual");
+            return false;
+        } else {
+            TextConfirmpassword.setError(null);
+            return true;
+        }
     }
 }
