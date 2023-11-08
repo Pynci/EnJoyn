d2373c22b6f6fe794a5ba12158f506313933dd25
stefanospinelli29 <128166491+stefanospinelli29@users.noreply.github.com>
2023-11-08 23:20:26 +0100

    Aggiunto controllo nome e cognome e migliorati output di errore

--- app/src/main/java/it/unimib/enjoyn/RegisterActivity.java
+++ app/src/main/java/it/unimib/enjoyn/RegisterActivity.java
@@ -15,6 +15,8 @@ public class RegisterActivity extends AppCompatActivity {
     TextInputLayout Textpassword;
     TextInputLayout TextEmail;
     TextInputLayout TextConfirmpassword;
+    TextInputLayout TextName;
+    TextInputLayout TextSurname;
     Button ButtonRegister;
 
     private static final String TAG = RegisterActivity.class.getSimpleName();
@@ -26,17 +28,23 @@ public class RegisterActivity extends AppCompatActivity {
         Textpassword = (findViewById(R.id.insertPassword));
         ButtonRegister = findViewById(R.id.buttonRegister);
         TextEmail = findViewById(R.id.insertEmail);
+        TextName = findViewById(R.id.insertName);
+        TextSurname = findViewById(R.id.insertSurname);
         TextConfirmpassword = findViewById(R.id.confirmPassword);
         ButtonRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String name = TextName.getEditText().getText().toString();
+                String surname = TextSurname.getEditText().getText().toString();
                 String password = Textpassword.getEditText().getText().toString();
                 String email = TextEmail.getEditText().getText().toString();
                 String confirmPassword = TextConfirmpassword.getEditText().getText().toString();
-                boolean checkedConfirmPassword= false;
+                boolean checkedConfirmPassword = false;
+                boolean checkedName = checkSurName(name, TextName);
+                boolean checkedSurname = checkSurName(surname, TextSurname);
                 boolean checkedPassword = checkPassword(password);
                 if(checkedPassword){
-                    checkedConfirmPassword= checkConfirmPassword( password, confirmPassword);
+                    checkedConfirmPassword = checkConfirmPassword(password, confirmPassword);
                 }
                 boolean checkedEmail = checkEmail(email);
                 Log.d(TAG, "funziona" + checkedPassword);
@@ -51,32 +59,37 @@ public class RegisterActivity extends AppCompatActivity {
         boolean number = false;
         boolean specialChar = false;
         boolean capLetter = false;
-        if (password != null && password.length() >= 8) {
-            for (int i = 0; i < password.length(); i++) {
-                if (password.charAt(i) >= '0' && password.charAt(i) <= '9') {
-                    number = true;
-                }
-                if (password.charAt(i) >= 'A' && password.charAt(i) <= 'Z') {
-                    capLetter = true;
-                }
-                if (password.charAt(i) >= '!' && password.charAt(i) <= '/') {
-                    specialChar = true;
-                }
-
-            }
-
-        } else {
-            Textpassword.setError("@string/tooShortPassword");
+        if (password == null || password.length()==0) {
+            Textpassword.setError(getString(R.string.Stringnull));
             return false;
         }
+        if(password.length() < 8) {
+            Textpassword.setError(getString(R.string.tooShortPassword));
+            return false;
+        }
+
+        for (int i = 0; i < password.length(); i++) {
+            if (password.charAt(i) >= '0' && password.charAt(i) <= '9') {
+                number = true;
+            }
+            if (password.charAt(i) >= 'A' && password.charAt(i) <= 'Z') {
+                capLetter = true;
+            }
+            if (password.charAt(i) >= '!' && password.charAt(i) <= '/') {
+                specialChar = true;
+            }
+
+        }
+
+
         if (!number) {
-            Textpassword.setError("@string/numberMissingPassword");
+            Textpassword.setError(getString(R.string.numberMissingPassword));
         } else {
             if (!capLetter) {
-                Textpassword.setError("@string/upperCaseMissingPassword");
+                Textpassword.setError(getString(R.string.upperCaseMissingPassword));
             } else {
                 if (!specialChar) {
-                    Textpassword.setError("@string/specialCharacterMissingPassword");
+                    Textpassword.setError(getString(R.string.specialCharacterMissingPassword));
                 }
             }
 
@@ -89,8 +102,12 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     private boolean checkEmail(String email) {
+        if(email==null || email.length()==0) {
+            TextEmail.setError(getString(R.string.Stringnull));
+            return false;
+        }
         if (!(EmailValidator.getInstance().isValid(email))) {
-            TextEmail.setError("@string/notValidEmail");
+            TextEmail.setError(getString(R.string.notValidEmail));
             return false;
         } else {
             TextEmail.setError(null);
@@ -98,12 +115,36 @@ public class RegisterActivity extends AppCompatActivity {
         }
     }
     private boolean checkConfirmPassword(String password, String confirmPassword) {
+        if(confirmPassword==null || confirmPassword.length()==0) {
+            TextConfirmpassword.setError(getString(R.string.Stringnull));
+            return false;
+        }
         if (!(confirmPassword.equals(password))) {
-            TextConfirmpassword.setError("@string/PasswordNotEqual");
+            TextConfirmpassword.setError(getString(R.string.passwordNotEqual));
             return false;
         } else {
             TextConfirmpassword.setError(null);
             return true;
         }
     }
+    private boolean checkSurName(String name, TextInputLayout Text) {
+
+        if (name == null || name.length()==0) {
+            Text.setError(getString(R.string.Stringnull));
+            return false;}
+        if( name.length()>=42) {
+            Text.setError(getString(R.string.StringTooLong));
+            return false;
+        }
+        for (int i = 0; i < name.length(); i++) {
+            if (!(name.charAt(i) >= 'A' && name.charAt(i) <= 'Z') && !(name.charAt(i) >= 'a' && name.charAt(i) <= 'z') && !(name.charAt(i) == 32)) {
+                Text.setError(getString(R.string.notSerious));
+                return false;
+            }
+        }
+        Text.setError(null);
+        return true;
+
+        }
+
 }
