3a1b96a422371f28cd739eb2d64a61d4517c530f
Francesco Trolli <franz.trolli@gmail.com>
2023-11-13 22:12:14 +0100

    Trasformazione da RegisterActivity a Fragment, creazione e implementazione Navigazion Graph

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,8 @@ dependencies {
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("commons-validator:commons-validator:1.7")
+    implementation("androidx.navigation:navigation-fragment:2.7.5")
+    implementation("androidx.navigation:navigation-ui:2.7.5")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
