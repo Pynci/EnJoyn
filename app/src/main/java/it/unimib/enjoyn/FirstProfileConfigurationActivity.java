75655a8e270145e2ab599c9f2d32cfbab962e2dd
Luca Pinciroli <l.pinciroli3@campus.unimib.it>
2023-11-24 19:32:35 +0100

    Abbozzata la grafica della prima personalizzazione profilo

--- app/src/main/java/it/unimib/enjoyn/FirstProfileConfigurationActivity.java
+++ app/src/main/java/it/unimib/enjoyn/FirstProfileConfigurationActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ProfileConfigurationActivity extends AppCompatActivity {
+public class FirstProfileConfigurationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile_configuration);
+        setContentView(R.layout.first_activity_profile_configuration);
     }
 }
