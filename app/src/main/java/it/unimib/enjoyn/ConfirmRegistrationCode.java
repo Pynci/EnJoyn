c1d95b02fa9b13b4fa89c74919d1683194c9cc4d
Marco Ferioli <m.ferioli@campus.unimib.it>
2023-11-28 17:14:37 +0100

    Completata grafica codice OTP per conferma profilo

--- app/src/main/java/it/unimib/enjoyn/ConfirmRegistrationCode.java
+++ app/src/main/java/it/unimib/enjoyn/ConfirmRegistrationCode.java
@@ -7,6 +7,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.chaos.view.PinView;
 
 public class ConfirmRegistrationCode extends Fragment {
 
@@ -47,6 +51,8 @@ public class ConfirmRegistrationCode extends Fragment {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
+
+
         }
     }
 
