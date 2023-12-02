16a0db8af6190a2d1bf8cc42e803c1f6c6bf71ae
Marco Ferioli <m.ferioli@campus.unimib.it>
2023-12-02 18:32:16 +0100

    Effettuate modifiche su pagina Registrazione.

--- app/src/main/java/it/unimib/enjoyn/LoginFragment.java
+++ app/src/main/java/it/unimib/enjoyn/LoginFragment.java
@@ -28,22 +28,18 @@ import it.unimib.enjoyn.util.SharedPreferencesUtil;
 
 public class LoginFragment extends Fragment {
 
-    Button ButtonLoginToRegister;
+    Button buttonLoginToRegister;
+    Button buttonLogin;
+    TextInputLayout textInputLayoutEmail;
+    TextInputLayout textInputLayoutPassword;
+    EditText editTextMail;
+    EditText editTextPassword;
 
-    Button LoginButton;
-
-    TextInputLayout TextEmailLogin;
-
-    TextInputLayout TextPasswordLogin;
-
-    EditText Email;
-
-    final String KEY_EMAIL="email";
     private static final boolean USE_NAVIGATION_COMPONENT = true;
+
     public static LoginFragment newInstance() {
         return new LoginFragment();
     }
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -64,62 +60,73 @@ public class LoginFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        ButtonLoginToRegister = view.findViewById(R.id.fragmentLogin_button_toLoginPage);
-        LoginButton = view.findViewById(R.id.buttonLogin);
-        TextEmailLogin = view.findViewById(R.id.fragmentLogin_textInputLayout_Email);
-        TextPasswordLogin = view.findViewById(R.id.fragmentLogin_textInputLayout_Password);
-        Email = view.findViewById(R.id.fragmentLogin_textInputEditText_Email);
+        buttonLoginToRegister = view.findViewById(R.id.fragmentLogin_button_toLoginPage);
+        buttonLogin = view.findViewById(R.id.buttonLogin);
 
+        textInputLayoutEmail = view.findViewById(R.id.fragmentLogin_textInputLayout_Email);
+        textInputLayoutPassword = view.findViewById(R.id.fragmentLogin_textInputLayout_Password);
+
+        editTextMail = view.findViewById(R.id.fragmentLogin_textInputEditText_Email);
+        editTextPassword = view.findViewById(R.id.fragmentLogin_textInputEditText_Password);
+
+        /*
         DataEncryptionUtil dataEncryptionUtil = new DataEncryptionUtil(requireContext());
         try {
             if (!dataEncryptionUtil.readSecretDataOnFile(ENCRYPTED_DATA_FILE_NAME).isEmpty()) {
                 SharedPreferencesUtil sharedPreferencesUtil = new SharedPreferencesUtil(requireContext());
-
-
             }
         } catch (GeneralSecurityException | IOException e) {
             e.printStackTrace();
         }
+         */
 
-      Email.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if(!hasFocus){
-                    String email = Email.getText().toString();
-                    checkEmail(email);
-                }
-                else {
-                    TextEmailLogin.setError(null);
-                }
+
+        /*
+        Serve a controllare che l'uente abbia inserito correttamente la mail.
+         */
+        editTextMail.setOnFocusChangeListener((v, hasFocus) -> {
+            if(!hasFocus){
+                checkEmail(this.editTextMail.getText().toString());
             }
-        });
-        ButtonLoginToRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivityBasedOnCondition(RegisterActivity.class,
-                        R.id.action_loginFragment_to_registerActivity, true);
+            else {
+                textInputLayoutEmail.setError(null);
             }
         });
 
-        LoginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String password = TextPasswordLogin.getEditText().getText().toString();
-                String email = TextEmailLogin.getEditText().getText().toString();
-                boolean checkedEmail = checkEmail(email);
-                boolean checkedPassword = checkPassword(password);
-                startActivityBasedOnCondition(MainButtonMenuActivity.class,
-                        R.id.action_loginFragment_to_mainButtonMenuActivity, true);
-                /**
-                 if(checkedEmail && checkedPassword){
-                    startActivityBasedOnCondition(MainButtonMenuActivity.class,
-                            R.id.action_loginFragment_to_mainButtonMenuActivity, true);
-                }
-                 */
+        /*
+        Serve a controllare che l'uente abbia inserito correttamente la password.
+         */
+        editTextPassword.setOnFocusChangeListener((v, hasFocus) -> {
+            if(!hasFocus){
+                checkPassword(this.editTextPassword.getText().toString());
             }
+            else {
+                textInputLayoutPassword.setError(null);
+            }
+        });
+
+
+        buttonLoginToRegister.setOnClickListener(v -> startActivityBasedOnCondition(RegisterActivity.class,
+                R.id.action_loginFragment_to_registerActivity, true));
+
+        /*
+        buttonLogin.setOnClickListener(v -> {
+            String password = textInputLayoutPassword.getEditText().getText().toString();
+            String email = textInputLayoutEmail.getEditText().getText().toString();
+            boolean checkedEmail = checkEmail(email);
+            boolean checkedPassword = checkPassword(password);
+            startActivityBasedOnCondition(MainButtonMenuActivity.class,
+                    R.id.action_loginFragment_to_mainButtonMenuActivity, true);
+
+             if(checkedEmail && checkedPassword){
+             startActivityBasedOnCondition(MainButtonMenuActivity.class,
+             R.id.action_loginFragment_to_mainButtonMenuActivity, true);
+             }
+
         }
 
         );
+         */
     }
 
     private void startActivityBasedOnCondition(Class<?> destinationActivity, int destination, boolean finishActivity) {
@@ -136,25 +143,25 @@ public class LoginFragment extends Fragment {
     }
 
     private boolean checkEmail(String email) {
-        if(email==null || email.length()==0) {
-            TextEmailLogin.setError(getString(R.string.Stringnull));
+        if(email == null || email.length() == 0) {
+            textInputLayoutEmail.setError(getString(R.string.Stringnull));
             return false;
         }
         if (!(EmailValidator.getInstance().isValid(email))) {
-            TextEmailLogin.setError(getString(R.string.notValidEmail));
+            textInputLayoutEmail.setError(getString(R.string.notValidEmail));
             return false;
         } else {
-            TextEmailLogin.setError(null);
+            textInputLayoutEmail.setError(null);
             return true;
         }
     }
 
     private boolean checkPassword(String password){
-        if(password==null || password.length()==0){
-            TextPasswordLogin.setError(getString(R.string.Stringnull));
+        if(password == null || password.length() == 0){
+            textInputLayoutPassword.setError(getString(R.string.Stringnull));
             return false;
         }
-        TextPasswordLogin.setError(null);
+        textInputLayoutPassword.setError(null);
         return true;
     }
 }
