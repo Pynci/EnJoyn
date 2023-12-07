ec9b15d3b780782c768e93c20678ba220548a234
Francesco Trolli <franz.trolli@gmail.com>
2023-12-07 17:39:38 +0100

    fix Scelta data in NewEventFragment

--- app/src/main/java/it/unimib/enjoyn/NewEventFragment.java
+++ app/src/main/java/it/unimib/enjoyn/NewEventFragment.java
@@ -1,5 +1,6 @@
 package it.unimib.enjoyn;
 
+import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.os.Bundle;
 
@@ -16,14 +17,13 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.DatePicker;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
 import java.util.Calendar;
 
 import it.unimib.enjoyn.util.DatePickerFragment;
-import it.unimib.enjoyn.util.TimePickerFragment;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -33,9 +33,9 @@ import it.unimib.enjoyn.util.TimePickerFragment;
 public class NewEventFragment extends Fragment {
 
     Button date;
+    TextView selectedDate;
 
     Button time;
-
     TextView selectedTime;
 
     // TODO: Rename parameter arguments, choose names that match
@@ -102,17 +102,49 @@ public class NewEventFragment extends Fragment {
         });
 
         date = view.findViewById(R.id.newEventFragment_button_datePicker);
+        selectedDate = view.findViewById(R.id.fragmentNewEvent_textView_date);
+
         FragmentManager fragmentManager = getParentFragmentManager();
 
+        // on below line we are adding click listener for our pick date button
         date.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                (new DatePickerFragment()).show(fragmentManager, "datePicker");
+                // on below line we are getting
+                // the instance of our calendar.
+                final Calendar c = Calendar.getInstance();
+
+                // on below line we are getting
+                // our day, month and year.
+                int year = c.get(Calendar.YEAR);
+                int month = c.get(Calendar.MONTH);
+                int day = c.get(Calendar.DAY_OF_MONTH);
+
+                // on below line we are creating a variable for date picker dialog.
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        // on below line we are passing context.
+                        date.getContext(),
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+                                // on below line we are setting date to our text view.
+                                selectedDate.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+
+                            }
+                        },
+                        // on below line we are passing year,
+                        // month and day for selected date in our date picker.
+                        year, month, day);
+                // at last we are calling show to
+                // display our date picker dialog.
+                datePickerDialog.show();
             }
         });
 
-        time = view.findViewById(R.id.pickTime);
-        selectedTime = view.findViewById(R.id.textViewdata);
+
+        time = view.findViewById(R.id.fragmentNewEvent_button_pickTime);
+        selectedTime = view.findViewById(R.id.fragmentNewEvent_textView_time);
 
         // on below line we are adding click
         // listener for our pick date button
