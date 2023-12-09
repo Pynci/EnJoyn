9a56a6445d0f0359ecf4b43e2d5ac7fe007b2b7c
Luca Pinciroli <l.pinciroli3@campus.unimib.it>
2023-12-09 18:10:04 +0100

    Refactoring

---
+++ app/src/main/java/it/unimib/enjoyn/PasswordResetFragment.java
@@ -0,0 +1,32 @@
+package it.unimib.enjoyn;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PasswordResetFragment extends Fragment {
+
+    public PasswordResetFragment() {
+        // Required empty public constructor
+    }
+
+    public static PasswordResetFragment newInstance() {
+        return new PasswordResetFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_password_reset, container, false);
+    }
+}
