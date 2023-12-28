3c9f1d6e965a9da3a0edf5f904f448fdfb7ce78a
Giulia Vitale <g.vitale16@campus.unimib.it>
2023-12-28 21:45:01 +0100

    implementata logica API, aggiunto event view model, DataSource per meteo, classe MeteoResponse e Result, manca parsing

--- app/src/main/java/it/unimib/enjoyn/model/MeteoApiResponse.java
+++ app/src/main/java/it/unimib/enjoyn/model/MeteoApiResponse.java
@@ -4,10 +4,27 @@ import java.util.List;
 
 public class MeteoApiResponse extends MeteoResponse{
 
+    double latitude;
+    double longitude;
+    double generationtime_ms;
+    double utc_offset_seconds;
+    String timezone;
+    String timezone_abbreviation;
+    double elevation;
+    Object minutely_15_units;
     private List<Meteo> meteo;
 
-    public MeteoApiResponse(List<Meteo> meteoList) {
+    public MeteoApiResponse(List<Meteo> meteoList, double latitude, double longitude, double generationtime_ms, double utc_offset_seconds, String timezone, String timezone_abbreviation, double elevation, Object minutely_15_units, List<Meteo> meteo) {
         super(meteoList);
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.generationtime_ms = generationtime_ms;
+        this.utc_offset_seconds = utc_offset_seconds;
+        this.timezone = timezone;
+        this.timezone_abbreviation = timezone_abbreviation;
+        this.elevation = elevation;
+        this.minutely_15_units = minutely_15_units;
+        this.meteo = meteo;
     }
 
 
